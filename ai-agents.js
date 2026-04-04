69638f094fb5b56de719883dc1a5f6a06b62c14a
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-04-05 02:19:02 +0600

    Task Execution Planning

--- ai-agents.js
+++ ai-agents.js
@@ -16,4 +16,23 @@ async function main() {
 
   console.log(completion.choices[0].message)
 }
-main();
+const analyzeGoal = async() => {
+    const goalText = "Learn python";
+    const durationDays = "7";
+    const promt = `User wants to achieve the goal of ${goalText} in ${durationDays} days. Analyze the goal and break it down into smaller, manageable tasks that can be completed within the given timeframe. Provide a step-by-step plan to achieve the goal, including specific actions and milestones.`
+
+    try{
+        const completion = await openai.chat.completions.create({
+        model: "stepfun/step-3.5-flash:free",
+        messages: [
+      { role: "user", content: promt }
+        ],
+    })
+    console.log(completion.choices[0].message)
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+analyzeGoal()
